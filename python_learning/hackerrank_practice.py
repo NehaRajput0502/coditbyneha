92d57a17a0617791fab8c8753eb3c76ddcc36da4
NehaRajput0502 <neharajput0502@gmail.com>
2025-12-04 16:53:41 +0530

    create files

--- python_learning/hackerrank_practice.py
+++ python_learning/hackerrank_practice.py
@@ -110,12 +110,3 @@ if __name__ == '__main__':
             lst.pop()
         elif command[0] == "reverse":
             lst.reverse()   
-
-#Write a function
-def is_leap(year):
-    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-        return True
-    else:
-        return False
-year = int(input())
-print(is_leap(year))
