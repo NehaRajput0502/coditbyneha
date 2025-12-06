8ac27631345a777e4eeef099bd6be6292d5f6f2c
NehaRajput0502 <neharajput0502@gmail.com>
2025-12-06 15:48:14 +0530

    add data in files

--- python_learning/day4567.py
+++ python_learning/day4567.py
@@ -13,16 +13,4 @@ def factorial(n):
 num = int(input("Enter a number to find its factorial: "))
 print("Factorial of", num, "is", factorial(num))
 
-# Check if a number is prime
-def is_prime(num):
-    if num <= 1:
-        return False
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
-number = int(input("Enter a number to check if it's prime: "))
-if is_prime(number):
-    print(number, "is a prime number.")
-else:
-    print(number, "is not a prime number.")
+
