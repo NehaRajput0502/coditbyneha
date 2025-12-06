6950177205facc61326d2d4d66a202044232ec9d
NehaRajput0502 <neharajput0502@gmail.com>
2025-12-06 15:48:44 +0530

    add data in files

--- python_learning/day123.py
+++ python_learning/day123.py
@@ -113,3 +113,16 @@ if __name__ == '__main__':
             lst.pop()
         elif command[0] == "reverse":
             lst.reverse()   
+
+# Finding largest number among three numbers
+x = int(input("Enter first number: "))  
+y = int(input("Enter second number: "))  
+z = int(input("Enter third number: "))
+if (x >= y) and (x >= z):
+    largest = x
+elif (y >= x) and (y >= z):
+    largest = y
+else:
+    largest = z
+print("The largest number is:", largest)
+            
