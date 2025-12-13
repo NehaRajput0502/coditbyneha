52b665b35a866323a34481094572039dbfdab3bc
NehaRajput0502 <neharajput0502@gmail.com>
2025-12-13 12:17:04 +0530

    add data in files

--- python_learning/day123.py
+++ python_learning/day123.py
@@ -221,4 +221,4 @@ print(int(d))        # typecast to integer and print
 #4.TypeCast and Double It
 ##Typecast "5" to int and then double it 5 * 2 = 10
 num = int(input())
-print(num*2
+print(num*2)
